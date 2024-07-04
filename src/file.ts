2c1dae18776a2f35fbb8cd8217567396bc7bdfe4
fernandoYtkg01 <fernando68ribeirosilva@gmail.com>
2024-07-03 22:09:35 -0300

    lendo um arquivo e escrenvendo em um arquivo.

--- src/file.ts
+++ src/file.ts
@@ -1,9 +1,26 @@
-import { writeFile } from "fs/promises"
+import { readFile, writeFile } from "fs/promises"
 
+// excrenvendo em um arquivo
+// async function exec() {
+//     const list = ['Pedro', 'lucas', 'João']
+//     const listtxt = list.join('\n')
+//     await writeFile('./teste.txt', listtxt)
+// }
+
+// exec()
+
+// lendo um arquivo é gravando 
 async function exec() {
-    const list = ['Pedro', 'lucas', 'João']
-    const listtxt = list.join('\n')
-    await writeFile('./teste.txt', listtxt)
+    const fileName = './teste.txt'
+    const fileContent = await readFile(fileName, { encoding: 'utf8' })
+
+    const list = fileContent.split('\n')
+    list.push('Fulano')
+
+    const fileTxt = list.join('\n')
+    console.log(fileTxt)
+
+    await writeFile(fileName, fileTxt)
 }
 
 exec()
